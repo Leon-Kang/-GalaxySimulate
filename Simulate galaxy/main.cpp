8ca4727b3ac8d44201fa6dd1f536be367ec52fef
Leon Kang <coderleonkang@gmail.com>
2017-07-20 15:57:16 +0800

    Initial Commit

---
+++ Simulate galaxy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simulate galaxy
+//
+//  Created by Leon Kang on 2017/7/20.
+//  Copyright © 2017年 Leon Kang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
